2eb538e72e42f55f996e927a5b509ffd6e1d9b9e
Dominique Budano <d.budano@gmail.com>
2023-05-13 10:43:48 +0200

    [fix] Remove onMounting

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <TresCanvas v-if="isMounted" clear-color="#82DBC5" window-size preset="realistic" style="height:100%; width:100%">
+    <TresCanvas clear-color="#82DBC5" window-size preset="realistic" style="height:100%; width:100%">
         <OrbitControls/>
         <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
         <Box :args="[1, 1, 1]" color="orange" />
@@ -10,12 +10,5 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core';
 import { OrbitControls, Box } from '@tresjs/cientos'
-import { onMounted } from 'vue';
-import { ref } from 'vue';
 
-const isMounted = ref(false);
-
-onMounted(() => {
-    isMounted.value = true;
-});
 </script>
